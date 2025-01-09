2f681f0e62f62214e4b8fb590dd6b07d4de1eac5
doingcodelikeashit <doingcodelikeashit@gmail.com>
2025-01-09 18:06:16 +0530

    Day-1 - variable and datatypes

---
+++ Day-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; // std is class
+int main(){
+    cout << "Hello World" << endl; // cout is object of std //endl used to break the line (end the line) in console
+    //Datatypes
+    int a = 10;
+    float b = 1.3;
+    char c = 'a';
+    string s = "Prince"; // we can also do chat[7]
+    bool x = true;
+    const float pi = 3.14;// we can not change constant variable's value // and we have to initilize value while defining variable
+
+    //arithmatec operators 
+    int vinit = 20;
+    int rutvik =10;
+
+    cout << vinit + rutvik << endl;
+    cout << vinit - rutvik << endl;
+    cout << vinit * rutvik << endl;
+    cout << vinit / rutvik << endl;
+    cout << vinit % rutvik << endl; // mod returns reminder of division
+}
